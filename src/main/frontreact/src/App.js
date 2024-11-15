b7fda9299a0449f725d18e950056893746815c63
yeoni5 <yeoni5@naver.com>
2024-11-15 14:50:51 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : ３</h3>
+      <img src="./image/망상토끼.gif" alt="망상토끼" />
+    </div>
+  );
+};
+
+export default App;
